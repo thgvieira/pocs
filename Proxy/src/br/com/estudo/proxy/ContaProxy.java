50fc4f7bf292da1e27264fa6b20f5ffe9a6290b9
thgvieira <thg.vieira12@gmail.com>
2014-06-17 09:25:29 -0400

    deletando projeto proxy

---
+++ Proxy/src/br/com/estudo/proxy/ContaProxy.java
@@ -0,0 +1,30 @@
+package br.com.estudo.proxy;
+
+import br.com.estudo.modelo.Conta;
+
+public class ContaProxy implements Conta {
+
+	private Conta conta;
+
+	public ContaProxy(Conta conta) {
+		this.conta = conta;
+	}
+
+	@Override
+	public void deposita(double valor) {
+		System.out.println("Deposito de "+valor+" registrado.");
+		this.conta.deposita(valor);
+	}
+
+	@Override
+	public void saca(double valor) {
+		System.out.println("Saque de "+valor+" registrado.");
+		this.conta.saca(valor);
+	}
+
+	@Override
+	public double getSaldo() {
+		return this.conta.getSaldo();
+	}
+
+}
