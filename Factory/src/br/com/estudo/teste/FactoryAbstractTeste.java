8ca4e2dadd29b50042592013a5a452cbdb75cf79
thgvieira <thg.vieira12@gmail.com>
2014-06-17 11:10:16 -0400

    Testes com JUnit

--- Factory/src/br/com/estudo/teste/FactoryAbstractTeste.java
+++ Factory/src/br/com/estudo/teste/FactoryAbstractTeste.java
@@ -1,29 +1,51 @@
 package br.com.estudo.teste;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.com.estudo.factory.FactoryAbstractProxy;
 import br.com.estudo.modelo.Conta;
 import br.com.estudo.modelo.ContaFisica;
 import br.com.estudo.modelo.ContaJuridica;
 
 public class FactoryAbstractTeste {
-
-	public static void main(String[] args) {
+	
+	@Test
+	public void factoryAbstractProxyContaFisica(){
 		Conta conta = FactoryAbstractProxy.getContaProxy(ContaFisica.class);
 
 		conta.deposita(1000.0);
 		conta.saca(200.0);
 		conta.saca(200.0);
-
-		System.out.println(conta.getSaldo());
 		
-		conta = FactoryAbstractProxy.getContaProxy(ContaJuridica.class);
-
-		conta.deposita(2000.0);
-		conta.saca(1200.0);
-		conta.saca(1800.0);
-
-		System.out.println(conta.getSaldo());
-
+		Assert.assertTrue(conta.getSaldo() == 600);
 	}
+	
+	@Test(expected = RuntimeException.class)
+	public void factoryAbstractProxyContaFisicaEstouroLimite(){
+		Conta conta = FactoryAbstractProxy.getContaProxy(ContaFisica.class);
 
+		conta.deposita(1000.0);
+		conta.saca(1600.0);		
+	}
+	
+	@Test
+	public void factoryAbstractProxyContaJuridica(){
+		Conta conta = FactoryAbstractProxy.getContaProxy(ContaJuridica.class);
+
+		conta.deposita(1000.0);
+		conta.saca(200.0);
+		conta.saca(200.0);
+		
+		Assert.assertTrue(conta.getSaldo() == 600);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void factoryAbstractProxyContaJuridicaEstouroLimite(){
+		Conta conta = FactoryAbstractProxy.getContaProxy(ContaJuridica.class);
+
+		conta.deposita(1000.0);
+		conta.saca(3100.0);		
+	}
+	
 }
